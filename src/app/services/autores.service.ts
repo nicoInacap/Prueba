846613b9baa00b07986faf78790dd92fe34ce494
DiegoParancan <diegoparancan1998@gmail.com>
2025-07-16 14:32:03 -0400

    diego

---
+++ src/app/services/autores.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export interface Autores {
+  id_autor?: number;
+  nombre: string;
+  correo: string;
+  contraseña: string;
+}
+@Injectable({
+  providedIn: "root"
+})
+export class AutoresService {
+  private apiUrl = "https://apiclases.inacode.cl/biblioteca/autores";
+
+  constructor(private http: HttpClient) {}
+
+  obtenerAutores(): Observable<Autores[]> {
+    return this.http.get<Autores[]>(this.apiUrl);
+  }
+
+  crearAutores(autor: Autores): Observable<any> {
+    return this.http.post(this.apiUrl, autor);
+  }
+
+  actualizarAutores(id: number, autor: Autores): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, autor);
+  }
+
+  eliminarAutores(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
