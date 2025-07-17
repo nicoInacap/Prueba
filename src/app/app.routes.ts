fe387565b0e2e4d792d5cf8c584ad923a3a954bd
DiegoParancan <diegoparancan1998@gmail.com>
2025-07-17 19:17:29 -0400

    aa5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LibrosComponent } from './pages/libros/libros';
 import { UsuariosComponent } from './pages/usuarios/usuarios';
+import { PrestamosComponent } from './pages/prestamos/prestamos';
+import { AutoresComponent } from './pages/autores/autores';
 
 
 export const routes: Routes = [
     {path: 'usuarios', component: UsuariosComponent},
-    {path: 'libros', component: LibrosComponent}
+    {path: 'libros', component: LibrosComponent},
+    {path: 'autores', component: AutoresComponent},
+    {path: 'prestamos', component: PrestamosComponent}
 ];
 
