4d97937765c29b4d07d38e3dada89edda0c1c5d8
DiegoParancan <diegoparancan1998@gmail.com>
2025-07-17 16:50:19 -0400

    aa2

---
+++ src/app/pages/prestamos/prestamos.ts
@@ -0,0 +1,79 @@
+import { Component,OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Prestamos, PrestamosService } from "../../services/prestamos.service";
+
+@Component({
+  selector: "app-prestamos",
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./prestamos.html",
+  styleUrl: "./prestamos.css"
+})
+export class PrestamosComponent implements OnInit {
+  prestamos: Prestamos[] = [];
+  prestamoNuevo: Prestamos = {
+    id_libro: '',
+    id_usuario: ''
+  };
+  modoEdicion = false;
+  idPrestamoEditando: number | null = null;
+
+  constructor(private prestamosSrv: PrestamosService) {}
+
+  ngOnInit(): void {
+    this.cargarPrestamos();
+  }
+
+  cargarPrestamos(): void {
+    this.prestamosSrv.obtenerPrestamos().subscribe({
+      next: (data: Prestamos[]) => {
+        this.prestamos = data;
+      },
+      error: (err: any) => {
+        console.error('Error al cargar los préstamos:', err);
+      }
+    });
+  }
+
+  guardarPrestamos(): void {
+    if (this.modoEdicion && this.idPrestamoEditando != null) {
+      // Actualizar préstamo existente
+      this.prestamosSrv
+        .editarPrestamos(this.idPrestamoEditando, this.prestamoNuevo)
+        .subscribe(() => {
+          this.cargarPrestamos();
+          this.resetFormulario();
+        });
+    } else {
+      // Crear nuevo préstamo
+      this.prestamosSrv
+        .crearPrestamos(this.prestamoNuevo)
+        .subscribe(() => {
+          this.cargarPrestamos();
+          this.resetFormulario();
+        });
+    }
+  }
+
+  editarPrestamos(prestamo: Prestamos): void {
+    this.modoEdicion = true;
+    this.idPrestamoEditando = prestamo.id ?? null;
+    this.prestamoNuevo = { ...prestamo };
+  }
+
+  eliminarPrestamos(id: number): void {
+    this.prestamosSrv.eliminarPrestamos(id).subscribe(() => {
+      this.cargarPrestamos();
+    });
+  }
+
+  resetFormulario(): void {
+    this.modoEdicion = false;
+    this.idPrestamoEditando = null;
+    this.prestamoNuevo = {
+      id_libro: '',
+      id_usuario: ''
+    };
+  }
+}
---
+++ src/app/services/prestamos.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export interface Prestamos {
+  id?: number;
+  id_libro: string;
+  id_usuario: string;
+}
+@Injectable({
+  providedIn: "root"
+})
+export class PrestamosService {
+  private apiUrl = "https://apiclases.inacode.cl/biblioteca/prestamos";
+
+  constructor(private http: HttpClient) {}
+
+  obtenerPrestamos(): Observable<Prestamos[]> {
+    return this.http.get<Prestamos[]>(this.apiUrl);
+  }
+
+  crearPrestamos(prestamo: Prestamos): Observable<any> {
+    return this.http.post(this.apiUrl, prestamo);
+  }
+
+  editarPrestamos(id: number, prestamo: Prestamos): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, prestamo);
+  }
+
+  eliminarPrestamos(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
+    
